f4775ddfb48c654002606eb8f4afd0a4d5bcf7bf
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2023-02-14 00:44:46 -0500

    Sección 15 finalizada

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,32 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createHashRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import { LoginPage } from '../auth/pages/LoginPage';
 import {
   HeroesRoutes,
   HeroesRoutesLayout,
 } from '../heroes/routes/HeroesRoutes';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: 'login',
-    element: <LoginPage />,
+    element: (
+      <PublicRoute>
+        <LoginPage />
+      </PublicRoute>
+    ),
   },
   {
     path: '/',
-    element: <HeroesRoutesLayout />,
+    element: (
+      <PrivateRoute>
+        <HeroesRoutesLayout />
+      </PrivateRoute>
+    ),
     children: HeroesRoutes,
   },
 ]);
--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,8 +1,12 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 export const Navbar = () => {
+  const { user, logout } = useContext(AuthContext);
   const navigate = useNavigate();
   const onLogout = () => {
+    logout();
     navigate('/login', { replace: true });
   };
 
@@ -46,16 +50,7 @@ export const Navbar = () => {
 
         <div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
           <ul className='navbar-nav ml-auto'>
-            {/* <NavLink
-              className={({ isActive }) =>
-                `nav-item nav-link ${isActive ? 'active' : ''}`
-              }
-              to='/login'
-            >
-              Logout
-            </NavLink> */}
-            <span className='nav-item nav-link text-info'>Andrés</span>
-
+            <span className='nav-item nav-link text-info'>{user?.name}</span>
             <button
               className='nav-item nav-link btn btn-outline-info'
               onClick={onLogout}
